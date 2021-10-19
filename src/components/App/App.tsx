5f3f8925965c0f37307267d2b00ebec0a2b3a658
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-10-20 02:46:39 +0300

    React.Suspense, React.lazy

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,12 @@
-import React, {ComponentType} from 'react';
+import React, {ComponentType, Suspense} from 'react';
 import styles from './App.module.css';
-import {BrowserRouter, Route, withRouter} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, withRouter} from 'react-router-dom';
 import Navbar from "../Navbar/Navbar";
-import UsersContainer from "../Users/UsersContainer";
 import News from "../News/News";
 import Music from "../Music/Music";
 import Photo from "../Photo/Photo";
 import Video from "../Video/Video";
 import Settings from "../Settings/Settings";
-import DialogsContainer from "../Dialogs/DialogsContainer";
-import ProfileContainer from "../Profile/ProfileContainer";
 import HeaderContainer from "../Header/HeaderContainer";
 import Login from "../Login/Login";
 import {connect, Provider} from "react-redux";
@@ -17,6 +14,12 @@ import {AppStateType, store} from "../../redux/reduxStore";
 import {compose} from "redux";
 import {initializeApp} from "../../redux/appReducer";
 import Preloader from "../Common/Preloader/Preloader";
+// import DialogsContainer from "../Dialogs/DialogsContainer";
+// import ProfileContainer from "../Profile/ProfileContainer";
+// import UsersContainer from "../Users/UsersContainer";
+const DialogsContainer = React.lazy(() => import('../Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('../Profile/ProfileContainer'));
+const UsersContainer = React.lazy(() => import('../Users/UsersContainer'));
 
 
 type MapDispatchType = {
@@ -39,7 +42,6 @@ class App extends React.Component<PropsType, StateType> {
         if (!this.props.initialized) {
             return <Preloader/>
         }
-
         return (
             <div className={styles.container}>
                 <div className={styles.app__wrapper}>
@@ -47,15 +49,19 @@ class App extends React.Component<PropsType, StateType> {
                     <Navbar/>
                     <div className={styles.content__wrapper}>
                         <div className={styles.content__inner}>
-                            <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
-                            <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
-                            <Route path={'/users'} render={() => <UsersContainer/>}/>
-                            <Route path={'/news'} render={() => <News/>}/>
-                            <Route path={'/music'} render={() => <Music/>}/>
-                            <Route path={'/photo'} render={() => <Photo/>}/>
-                            <Route path={'/video'} render={() => <Video/>}/>
-                            <Route path={'/settings'} render={() => <Settings/>}/>
-                            <Route path={'/login'} render={() => <Login/>}/>
+                            <Suspense fallback={<Preloader/>}>
+                                <Switch>
+                                    <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
+                                    <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
+                                    <Route path={'/users'} render={() => <UsersContainer/>}/>
+                                    <Route path={'/news'} render={() => <News/>}/>
+                                    <Route path={'/music'} render={() => <Music/>}/>
+                                    <Route path={'/photo'} render={() => <Photo/>}/>
+                                    <Route path={'/video'} render={() => <Video/>}/>
+                                    <Route path={'/settings'} render={() => <Settings/>}/>
+                                    <Route path={'/login'} render={() => <Login/>}/>
+                                </Switch>
+                            </Suspense>
                         </div>
                     </div>
                 </div>
@@ -72,7 +78,7 @@ let AppContainer = compose<ComponentType>(
     withRouter,
     connect(mstp, {initializeApp}))(App)
 
-let SocialApp = (props: StateType) => {
+let SocialApp = () => {
     return (
         <BrowserRouter>
             <Provider store={store}>
